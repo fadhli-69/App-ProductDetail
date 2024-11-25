f421e27692258f8c391ff9b8f43b32167bdf7202
Muhammad Nurfadhli <79645149+mozart-69@users.noreply.github.com>
2024-11-26 01:49:17 +0700

    menambahkan fitur talkback

--- app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
@@ -53,7 +53,29 @@ class MainActivity : AppCompatActivity() {
                 descTextView.text = desc
                 priceTextView.text = price.withCurrencyFormat()
                 dateTextView.text = getString(R.string.dateFormat, date.withDateFormat())
-                ratingTextView.text = getString(R.string.ratingFormat, rating.withNumberingFormat(), countRating.withNumberingFormat())
+                ratingTextView.text = getString(
+                    R.string.ratingFormat,
+                    rating.withNumberingFormat(),
+                    countRating.withNumberingFormat()
+                )
+            }
+        }
+        setupAccessibility(product)
+    }
+
+    private fun setupAccessibility(productModel: ProductModel) {
+        productModel.apply {
+            binding.apply {
+                settingImageView.contentDescription = getString(R.string.settingDescription)
+                previewImageView.contentDescription = getString(R.string.previewDescription)
+                colorTextView.contentDescription = getString(R.string.colorDescription, color)
+                sizeTextView.contentDescription = getString(R.string.sizeDescription, size)
+                ratingTextView.contentDescription = getString(
+                    R.string.ratingDescription,
+                    rating.withNumberingFormat(),
+                    countRating.withNumberingFormat()
+                )
+                storeTextView.contentDescription = getString(R.string.storeDescription, store)
             }
         }
     }
